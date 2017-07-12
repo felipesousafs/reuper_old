3586080e4b9f3b6b7e0185010f71000e303b1031
Felipe Sousa <felipe.sousa.fs@gmail.com>
2017-07-12 11:47:57 -0300

    [ADD] Front-end

--- app/models/residente.rb
+++ app/models/residente.rb
@@ -1,6 +1,7 @@
 class Residente < ApplicationRecord
   belongs_to :curso
   belongs_to :quarto
+  validates_presence_of :nome, :curso_id, :quarto_id, :matricula
 
   def quarto
     @quarto = Quarto.find(self.quarto_id)
