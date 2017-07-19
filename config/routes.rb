54638fadfeb24e274ce381f7c9a162a9e13040fe
Felipe Sousa <felipe.sousa.fs@gmail.com>
2017-07-19 11:55:22 -0300

    Gerar PDF: WORKING!!!

--- config/routes.rb
+++ config/routes.rb
@@ -9,6 +9,7 @@ Rails.application.routes.draw do
   resources :periodos
 
   root :to => "residentes#index"
+  get 'tabela/lixo', to: 'downloads#show', as: 'tabela/lixo'
   get 'populate_lixotodos', to: 'application#populate_lixotodos', as: 'populate_lixotodos'
   get 'refresh_lixodones', to: 'application#refresh_lixodones', as: 'refresh_lixodones'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
