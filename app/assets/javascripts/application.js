18b5511f48cf965d65977ebc9d958517674d243a
Felipe Sousa <felipe.sousa.fs@gmail.com>
2017-07-21 11:51:33 -0300

    [ADD] Tooltips

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,6 +11,8 @@
 // about supported directives.
 //
 //= require jquery
+//= require bootstrap
+//= require bootstrap-sprockets
 //= require jquery_ujs
 //= require jquery-ui
 //= require jquery-ui/widgets/autocomplete
