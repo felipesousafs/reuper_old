654612ee1639ec6479c66a59c6bcb941d37df437
Felipe Sousa <felipe.sousa.fs@gmail.com>
2017-07-28 08:54:59 -0300

    Mudanças na criação da FK de Curso e Quarto em Residente

--- db/migrate/20170707115407_create_residentes.rb
+++ db/migrate/20170707115407_create_residentes.rb
@@ -3,14 +3,12 @@ class CreateResidentes < ActiveRecord::Migration[5.0]
     create_table :residentes do |t|
       t.string :matricula
       t.string :nome
-      t.integer :curso_id
-      t.integer :quarto_id
+      t.references :curso, foreign_key: true
+      t.references :quarto_id, foreign_key: true
 
       t.timestamps
     end
     add_index :residentes, :matricula
-    add_reference :residentes, :quarto_id, foreign_key: true
-    add_reference :residentes, :curso_id, foreign_key: true
 
   end
 end
